6f3bc05f68859fba17cfe47834c1ad8c60c99daf
vlad <vlad>
2021-04-04 10:41:53 +0300

    added docker files

--- login-app/login.js
+++ login-app/login.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('This is the login section');
-}).listen(8080);
+  res.end('Welcome! This is the login section');
+}).listen(8000);
